6567c165b8354b0583fb8bae6bb7ecc379de4bcf
ming luo <itestmycode@gmail.com>
2021-01-06 17:46:41 -0500

    multi OS and architecture build

---
+++ scripts/build-multi-os-arch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#
+# build releases for multiple OS and Architecture
+#
+
+# absolute directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+VERSION="$( cat ../Makefile  | grep "^TAG" | cut -d' ' -f3 )"
+
+function build_os_arch() {
+  OS=$1
+  ARCH=$2
+  echo "build for OS $1 ARCH $2"
+  GOOS=$OS GOARCH=$ARCH go build -o ${DIR}/../bin/releases/pulsar-heartbeat-$VERSION-$OS-$ARCH .
+  gzip ${DIR}/../bin/releases/pulsar-heartbeat-$VERSION-$OS-$ARCH
+}
+cd $DIR/../src
+# test lint, vet, and build as basic build steps in CI
+echo run golint
+golint ./...
+echo run go vet
+go vet ./...
+
+echo "run go build for version ${VERSION}"
+mkdir -p ${DIR}/../bin/releases
+rm -f ${DIR}/../bin/releases/pulsar-heartbeat*
+
+build_os_arch "linux" "amd64"
+build_os_arch "linux" "arm"
+build_os_arch "linux" "arm64"
+build_os_arch "linux" "ppc64"
+build_os_arch "darwin" "amd64"
+build_os_arch "windows" "amd64"
+build_os_arch "windows" "arm"
+build_os_arch "windows" "386"
+build_os_arch "solaris" "amd64"
+build_os_arch "freebsd" "amd64"
+build_os_arch "freebsd" "arm64"
+build_os_arch "openbsd" "amd64"
+build_os_arch "openbsd" "arm64"
+
+# Not supported
+# build_os_arch "darwin" "arm64"
