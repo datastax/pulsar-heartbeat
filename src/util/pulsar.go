a67a22d7ca5a6680afbbb8e4a8947aec8e74d6e6
Ming <itestmycode@gmail.com>
2021-12-17 18:16:18 -0500

    stop send error or message to a closed channel due to timeout (#20)
    * stop send error or message to a closed channel due to timeout
    
    * waitGroup to synchronize channel close

--- src/util/pulsar.go
+++ src/util/pulsar.go
@@ -1,23 +1,23 @@
- //
- //  Copyright (c) 2020-2021 Datastax, Inc.
- //  
- //  Licensed to the Apache Software Foundation (ASF) under one
- //  or more contributor license agreements.  See the NOTICE file
- //  distributed with this work for additional information
- //  regarding copyright ownership.  The ASF licenses this file
- //  to you under the Apache License, Version 2.0 (the
- //  "License"); you may not use this file except in compliance
- //  with the License.  You may obtain a copy of the License at
- //  
- //     http://www.apache.org/licenses/LICENSE-2.0
- //  
- //  Unless required by applicable law or agreed to in writing,
- //  software distributed under the License is distributed on an
- //  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- //  KIND, either express or implied.  See the License for the
- //  specific language governing permissions and limitations
- //  under the License.
- //
+//
+//  Copyright (c) 2020-2021 Datastax, Inc.
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
 
 package util
 
@@ -25,6 +25,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/apache/pulsar-client-go/pulsar"
@@ -40,7 +41,7 @@ type ConsumerResult struct {
 }
 
 // VerifyMessageByPulsarConsumer instantiates a Pulsar consumer and verifies an expected message
-func VerifyMessageByPulsarConsumer(client pulsar.Client, topicName, expectedMessage string, completeChan chan *ConsumerResult) error {
+func VerifyMessageByPulsarConsumer(client pulsar.Client, topicName, expectedMessage string, receiveTimeout time.Duration, wg *sync.WaitGroup, completeChan chan *ConsumerResult) error {
 	topicParts := strings.Split(topicName, "/")
 	subscriptionName := "partition-sub" + topicParts[len(topicParts)-1]
 	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
@@ -54,12 +55,15 @@ func VerifyMessageByPulsarConsumer(client pulsar.Client, topicName, expectedMess
 		log.Errorf("failed to created partition topic consumer, error: %v", err)
 		return err
 	}
-	defer consumer.Close()
+	wg.Add(1)
+	defer func() {
+		wg.Done()
+		consumer.Close()
+	}()
 
 	receivedCount := 0
-	receiveTimeout := 60 * time.Second
 	start := time.Now()
-	for time.Since(start) < 90*time.Second { // TODO: this should be passed in as timeout parameter
+	for time.Since(start) <= receiveTimeout {
 		cCtx, cancel := context.WithTimeout(context.Background(), receiveTimeout)
 		defer cancel()
 
