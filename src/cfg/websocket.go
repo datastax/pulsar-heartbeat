a3574556bfe90607ed0ab16968593ab460e53189
ming <itestmycode@gmail.com>
2022-02-14 20:43:51 -0500

    remove websocket alert based on standard deviation (#29)

--- src/cfg/websocket.go
+++ src/cfg/websocket.go
@@ -202,6 +202,7 @@ func TestWsLatency(config WsConfig) {
 	if err != nil {
 		errMsg := fmt.Sprintf("cluster %s, %s websocket latency test Pulsar error: %v", config.Cluster, config.Name, err)
 		log.Errorf(errMsg)
+		ReportIncident(config.Name, config.Cluster, "websocket persisted latency test failure", errMsg, &config.AlertPolicy)
 	} else if result.Latency > expectedLatency {
 		stdVerdict.Add(float64(result.Latency.Milliseconds()))
 		errMsg := fmt.Sprintf("cluster %s, %s websocket test message latency %v over the budget %v",
@@ -212,7 +213,6 @@ func TestWsLatency(config WsConfig) {
 		errMsg := fmt.Sprintf("cluster %s, websocket test message latency %v over three standard deviation %v ms and mean is %v ms",
 			config.Cluster, result.Latency, stddev, mean)
 		log.Errorf(errMsg)
-		ReportIncident(config.Name, config.Cluster, "websocket persisted latency test failure", errMsg, &config.AlertPolicy)
 	} else {
 		log.Infof("websocket pubsub succeeded with latency %v expected latency %v on topic %s, cluster %s\n",
 			result.Latency, expectedLatency, config.TopicName, config.Cluster)
